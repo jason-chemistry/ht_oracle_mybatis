633cd0ee16f612f36257acad878c9db382af7865
djn <867754014@qq.com>
2021-03-02 14:40:41 +0800

    全部提交

--- src/main/java/com/example/springboottest2/service/serviceImpl/CeshiServiceImpl.java
+++ src/main/java/com/example/springboottest2/service/serviceImpl/CeshiServiceImpl.java
@@ -23,8 +23,13 @@ public class CeshiServiceImpl implements CeshiService {
 
     @Override
     public String query() {
-        String i = ceshiDao.query();
-        return i;
+        String i = ceshiDao.getCsrq("1012565");
+        String days=i.substring(0,4)+"-"+i.substring(4,6)+"-"+i.substring(6,8);
+        String j =ceshiDao.getRyrq("1012565");
+        String dayss=j.substring(0,4)+"-"+j.substring(4,6)+"-"+j.substring(6,8)+" "+j.substring(8,10)+":"+j.substring(10,12);
+        String k =ceshiDao.getCyrq("1012565");
+        String daysss=k.substring(0,4)+"-"+k.substring(4,6)+"-"+k.substring(6,8)+" "+k.substring(8,10)+":"+k.substring(10,12);
+        return days+dayss+daysss;
     }
     @Override
     public List<String> getAllPid(){
@@ -37,6 +42,8 @@ public class CeshiServiceImpl implements CeshiService {
         List<String> bids=ceshiDao.getAllBid();
         return bids;
     }
+
+
     @Override
     public String getAllMessage() throws IOException {
         List<String> pids=ceshiDao.getAllPid();
@@ -53,20 +60,22 @@ public class CeshiServiceImpl implements CeshiService {
 
             person.setPid(p);
             person.setBid(b);
-            person.setAdminission_no(b);
+
             person.setSex(sexs.get(k));
             person.setDept(depts.get(k));
-            person.setName(names.get(k));
+
             person.setZkys(ceshiDao.getZkys(p));
             person.setZkhs(ceshiDao.getZkhs(p));
             person.setZzys(ceshiDao.getZzys(p));
             person.setZrhs(ceshiDao.getZrhs(p));
             person.setSfzh(ceshiDao.getSfzh(b));
-            person.setZzd(ceshiDao.getZzd(p));
+
             person.setZzddm(ceshiDao.getZzddm(p));
-            person.setCsrq(ceshiDao.getCsrq(p));
+
+            person.setCsrq(getCsrq(p));
             person.setRyrq(ceshiDao.getRyrq(p));
             person.setCyrq(ceshiDao.getCyrq(p));
+
             person.setFffs(ceshiDao.getFffs(p));
             person.setZytj(ceshiDao.getZytj(p));
             person.setDjczy(ceshiDao.getDjczy(p));
@@ -111,7 +120,7 @@ public class CeshiServiceImpl implements CeshiService {
             Long l=ceshiDao.getAdl(p);
             String m=String.valueOf(l);
             person.setAdl(m);
-            person.setBhxyssj(ceshiDao.getBhxyssj(p));
+            person.setBhxyssj(getBhxyssj(p));
             person.setBhxyscs(ceshiDao.getBhxyscs(p));
             person.setNorse(ceshiDao.getNorse(p));
             persons.add(person);
@@ -124,137 +133,131 @@ public class CeshiServiceImpl implements CeshiService {
 
         //  创建行
         HSSFRow row = sheet.createRow(0);
-        row.createCell(0).setCellValue("bid");
-        row.createCell(1).setCellValue("pid");
-        row.createCell(2).setCellValue("adminission_no");
-        row.createCell(3).setCellValue("name");
-        row.createCell(4).setCellValue("sex");
-        row.createCell(5).setCellValue("weight");
-        row.createCell(6).setCellValue("height");
-        row.createCell(7).setCellValue("dept");
-        row.createCell(8).setCellValue("zkys");
-        row.createCell(9).setCellValue("zkhs");
-        row.createCell(10).setCellValue("zzys");
-        row.createCell(11).setCellValue("zrhs");
-        row.createCell(12).setCellValue("sfzh");
-        row.createCell(13).setCellValue("zzd");
-        row.createCell(14).setCellValue("zzddm");
-        row.createCell(15).setCellValue("csrq");
-        row.createCell(16).setCellValue("ryrq");
-        row.createCell(17).setCellValue("cyrq");
-        row.createCell(18).setCellValue("fffs");
-        row.createCell(19).setCellValue("zytj");
-        row.createCell(20).setCellValue("djczy");
-        row.createCell(21).setCellValue("zyts");
-        row.createCell(22).setCellValue("lyfs");
-        row.createCell(23).setCellValue("zfy");
-        row.createCell(24).setCellValue("zfje");
-        row.createCell(25).setCellValue("ylfwf");
-        row.createCell(26).setCellValue("zlczf");
-        row.createCell(27).setCellValue("hlf");
-        row.createCell(28).setCellValue("zhyl");
-        row.createCell(29).setCellValue("blzdf");
-        row.createCell(30).setCellValue("syszdf");
-        row.createCell(31).setCellValue("yxxzdf");
-        row.createCell(32).setCellValue("lczdf");
-        row.createCell(33).setCellValue("fsszl");
-        row.createCell(34).setCellValue("lcwlzlfy");
-        row.createCell(35).setCellValue("sszlf");
-        row.createCell(36).setCellValue("mzf");
-        row.createCell(37).setCellValue("ssf");
-        row.createCell(38).setCellValue("kff");
-        row.createCell(39).setCellValue("zyzlf");
-        row.createCell(40).setCellValue("xyf");
-        row.createCell(41).setCellValue("kjywf");
-        row.createCell(42).setCellValue("zcyf_mix");
-        row.createCell(43).setCellValue("zcyf");
-        row.createCell(44).setCellValue("xf");
-        row.createCell(45).setCellValue("bdb");
-        row.createCell(46).setCellValue("qbd");
-        row.createCell(47).setCellValue("nxyz");
-        row.createCell(48).setCellValue("zbyz");
-        row.createCell(49).setCellValue("jcy");
-        row.createCell(50).setCellValue("zly");
-        row.createCell(51).setCellValue("ssy");
-        row.createCell(52).setCellValue("qt");
-        row.createCell(53).setCellValue("zs");
-        row.createCell(54).setCellValue("cydy");
-        row.createCell(55).setCellValue("wsgj");
-        row.createCell(56).setCellValue("zswxx");
-        row.createCell(57).setCellValue("adl");
-        row.createCell(58).setCellValue("bhxyssj");
-        row.createCell(59).setCellValue("bhxyscs");
-        row.createCell(60).setCellValue("nosie");
+        row.createCell(0).setCellValue("Number");
+        row.createCell(1).setCellValue("科室");//dept
+        row.createCell(2).setCellValue("PatientId");
+        row.createCell(3).setCellValue("性别");
+        row.createCell(4).setCellValue("身份证号");
+        row.createCell(5).setCellValue("身高");
+        row.createCell(6).setCellValue("体重");
+        row.createCell(7).setCellValue("诊断代码");
+        row.createCell(8).setCellValue("主诉");//主诉
+        row.createCell(9).setCellValue("质控医生");
+        row.createCell(10).setCellValue("质控护士");
+        row.createCell(11).setCellValue("主治医生");
+        row.createCell(12).setCellValue("责任护士");
+        row.createCell(13).setCellValue("出生日期");
+        row.createCell(14).setCellValue("入院日期");
+        row.createCell(15).setCellValue("出院日期");
+        row.createCell(16).setCellValue("付费方式");
+        row.createCell(17).setCellValue("住院途径");
+        row.createCell(18).setCellValue("第几次住院");
+        row.createCell(19).setCellValue("住院天数");
+        row.createCell(20).setCellValue("离院方式");
+        row.createCell(21).setCellValue("自杀危险性");
+        row.createCell(22).setCellValue("外显攻击行为");
+        row.createCell(23).setCellValue("ADL");
+        row.createCell(24).setCellValue("NOISE");//
+        row.createCell(25).setCellValue("保护性约束时间");//约束性
+        row.createCell(26).setCellValue("保护性约束次数");
+        row.createCell(27).setCellValue("总费用");//总费用
+        row.createCell(28).setCellValue("自费金额");
+        row.createCell(29).setCellValue("医疗服务费");
+        row.createCell(30).setCellValue("治疗操作费");//cure_operation
+        row.createCell(31).setCellValue("护理费");//护理费
+        row.createCell(32).setCellValue("综合医疗服务类其它费");
+        row.createCell(33).setCellValue("病例诊断费");
+        row.createCell(34).setCellValue("实验室诊断费");
+        row.createCell(35).setCellValue("影像学诊断费");
+        row.createCell(36).setCellValue("临床诊断费");
+        row.createCell(37).setCellValue("非手术治疗项目费");
+        row.createCell(38).setCellValue("其中临床物理治疗费");
+        row.createCell(39).setCellValue("手术治疗费");
+        row.createCell(40).setCellValue("其中麻醉费");
+        row.createCell(41).setCellValue("其中手术费");
+        row.createCell(42).setCellValue("康复费");
+        row.createCell(43).setCellValue("中医治疗费");
+        row.createCell(44).setCellValue("西药费");
+        row.createCell(45).setCellValue("其中抗菌药物费");
+        row.createCell(46).setCellValue("中成药费");
+        row.createCell(47).setCellValue("中草药费");
+        row.createCell(48).setCellValue("血费");
+        row.createCell(49).setCellValue("白蛋白类制品费");
+        row.createCell(50).setCellValue("球蛋白类制品费");
+        row.createCell(51).setCellValue("凝血因子类制品费");
+        row.createCell(52).setCellValue("细胞因子类制品费");
+        row.createCell(53).setCellValue("检查用一次性医用材料费");
+        row.createCell(54).setCellValue("治疗用一次性医用材料费");
+        row.createCell(55).setCellValue("手术用一次性医用材料费");
+        row.createCell(56).setCellValue("其他费");
+        row.createCell(57).setCellValue("出院带药");
 
 
 
 
 
-        for (int i = 1; i < persons.size(); i++) {
+        for (int i = 1; i < 5; i++) {
             HSSFRow rows = sheet.createRow(i);
             Person a=persons.get(i-1);
             for (int j = 0; j < 59; j = j + 1){
-                rows.createCell(0).setCellValue(a.getBid());
-                rows.createCell(1).setCellValue(a.getPid());
-                rows.createCell(2).setCellValue(a.getAdminission_no());
-                rows.createCell(3).setCellValue(a.getName());
-                rows.createCell(4).setCellValue(a.getSex());
-                rows.createCell(5).setCellValue(a.getWeight());
-                rows.createCell(6).setCellValue(a.getHeight());
-                rows.createCell(7).setCellValue(a.getDept());
-                rows.createCell(8).setCellValue(a.getZkys());
-                rows.createCell(9).setCellValue(a.getZkhs());
-                rows.createCell(10).setCellValue(a.getZzys());
-                rows.createCell(11).setCellValue(a.getZrhs());
-                rows.createCell(12).setCellValue(a.getSfzh());
-                rows.createCell(13).setCellValue(a.getZzd());
-                rows.createCell(14).setCellValue(a.getZzddm());
-                rows.createCell(15).setCellValue(a.getCsrq());
-                rows.createCell(16).setCellValue(a.getRyrq());
-                rows.createCell(17).setCellValue(a.getCyrq());
-                rows.createCell(18).setCellValue(a.getFffs());
-                rows.createCell(19).setCellValue(a.getZytj());
-                rows.createCell(20).setCellValue(a.getDjczy());
-                rows.createCell(21).setCellValue(a.getZyts());
-                rows.createCell(22).setCellValue(a.getLyfs());
-                rows.createCell(23).setCellValue(a.getZfy());
-                rows.createCell(24).setCellValue(a.getZfje());
-                rows.createCell(25).setCellValue(a.getYlfwf());
-                rows.createCell(26).setCellValue(a.getZlczf());
-                rows.createCell(27).setCellValue(a.getHlf());
-                rows.createCell(28).setCellValue(a.getZhyl());
-                rows.createCell(29).setCellValue(a.getBlzdf());
-                rows.createCell(30).setCellValue(a.getSyszdf());
-                rows.createCell(31).setCellValue(a.getYxxzdf());
-                rows.createCell(32).setCellValue(a.getLczdf());
-                rows.createCell(33).setCellValue(a.getFsszl());
-                rows.createCell(34).setCellValue(a.getLcwlzlfy());
-                rows.createCell(35).setCellValue(a.getSszlf());
-                rows.createCell(36).setCellValue(a.getMzf());
-                rows.createCell(37).setCellValue(a.getSsf());
-                rows.createCell(38).setCellValue(a.getKff());
-                rows.createCell(39).setCellValue(a.getZyzlf());
-                rows.createCell(40).setCellValue(a.getXyf());
-                rows.createCell(41).setCellValue(a.getKjywf());
-                rows.createCell(42).setCellValue(a.getZcyf_mix());
-                rows.createCell(43).setCellValue(a.getZcyf());
-                rows.createCell(44).setCellValue(a.getXf());
-                rows.createCell(45).setCellValue(a.getBdb());
-                rows.createCell(46).setCellValue(a.getQbd());
-                rows.createCell(47).setCellValue(a.getNxyz());
-                rows.createCell(48).setCellValue(a.getZbyz());
-                rows.createCell(49).setCellValue(a.getJcy());
-                rows.createCell(50).setCellValue(a.getZly());
-                rows.createCell(51).setCellValue(a.getSsy());
-                rows.createCell(52).setCellValue(a.getQt());
-                rows.createCell(53).setCellValue(a.getZs());
-                rows.createCell(54).setCellValue(a.getCydy());
-                rows.createCell(55).setCellValue(a.getWsgj());
-                rows.createCell(56).setCellValue(a.getZswxx());
-                rows.createCell(57).setCellValue(a.getAdl());
-                rows.createCell(58).setCellValue(a.getBhxyssj());
-                rows.createCell(59).setCellValue(a.getBhxyscs());
-                rows.createCell(60).setCellValue(a.getNorse());
+                rows.createCell(0).setCellValue(i);
+                rows.createCell(1).setCellValue(a.getDept());
+                rows.createCell(2).setCellValue(a.getPid());
+                rows.createCell(3).setCellValue(a.getSex());
+                rows.createCell(4).setCellValue(a.getSfzh());
+                rows.createCell(5).setCellValue(a.getHeight());
+                rows.createCell(6).setCellValue(a.getWeight());
+                rows.createCell(7).setCellValue(a.getZzddm());
+                rows.createCell(8).setCellValue(a.getZs());
+                rows.createCell(9).setCellValue(a.getZkys());
+                rows.createCell(10).setCellValue(a.getZkhs());
+                rows.createCell(11).setCellValue(a.getZzys());
+                rows.createCell(12).setCellValue(a.getZrhs());
+                rows.createCell(13).setCellValue(a.getCsrq());
+                rows.createCell(14).setCellValue(a.getRyrq());
+                rows.createCell(15).setCellValue(a.getCyrq());
+                rows.createCell(16).setCellValue(a.getFffs());
+                rows.createCell(17).setCellValue(a.getZytj());
+                rows.createCell(18).setCellValue(a.getDjczy());
+                rows.createCell(19).setCellValue(a.getZyts());
+                rows.createCell(20).setCellValue(a.getLyfs());
+                rows.createCell(21).setCellValue(a.getZswxx());
+                rows.createCell(22).setCellValue(a.getWsgj());
+                rows.createCell(23).setCellValue(a.getAdl());
+                rows.createCell(24).setCellValue(a.getNorse());
+                rows.createCell(25).setCellValue(a.getBhxyssj());
+                rows.createCell(26).setCellValue(a.getBhxyscs());
+                rows.createCell(27).setCellValue(a.getZfy());
+                rows.createCell(28).setCellValue(a.getZfje());
+                rows.createCell(29).setCellValue(a.getYlfwf());
+                rows.createCell(30).setCellValue(a.getZlczf());
+                rows.createCell(31).setCellValue(a.getHlf());
+                rows.createCell(32).setCellValue(a.getZhyl());
+                rows.createCell(33).setCellValue(a.getBlzdf());
+                rows.createCell(34).setCellValue(a.getSyszdf());
+                rows.createCell(35).setCellValue(a.getYxxzdf());
+                rows.createCell(36).setCellValue(a.getLczdf());
+                rows.createCell(37).setCellValue(a.getFsszl());
+                rows.createCell(38).setCellValue(a.getLcwlzlfy());
+                rows.createCell(39).setCellValue(a.getSszlf());
+                rows.createCell(40).setCellValue(a.getMzf());
+                rows.createCell(41).setCellValue(a.getSsf());
+                rows.createCell(42).setCellValue(a.getKff());
+                rows.createCell(43).setCellValue(a.getZyzlf());
+                rows.createCell(44).setCellValue(a.getXyf());
+                rows.createCell(45).setCellValue(a.getKjywf());
+                rows.createCell(46).setCellValue(a.getZcyf_mix());
+                rows.createCell(47).setCellValue(a.getZcyf());
+                rows.createCell(48).setCellValue(a.getXf());
+                rows.createCell(49).setCellValue(a.getBdb());
+                rows.createCell(50).setCellValue(a.getQbd());
+                rows.createCell(51).setCellValue(a.getNxyz());
+                rows.createCell(52).setCellValue(a.getZbyz());
+                rows.createCell(53).setCellValue(a.getJcy());
+                rows.createCell(54).setCellValue(a.getZly());
+                rows.createCell(55).setCellValue(a.getSsy());
+                rows.createCell(56).setCellValue(a.getQt());
+                rows.createCell(57).setCellValue(a.getCydy());
 
             }
         }
@@ -308,15 +311,21 @@ public class CeshiServiceImpl implements CeshiService {
 	
 	@Override
 	public String getCsrq(String pid){
-        return "1";
+        String i=ceshiDao.getCsrq(pid);
+        String days=i.substring(0,4)+"-"+i.substring(4,6)+"-"+i.substring(6,8);
+        return days;
 	}
 	@Override
 	public String getRyrq(String pid){
-        return "1";
+        String j =ceshiDao.getRyrq(pid);
+        String dayss=j.substring(0,4)+"-"+j.substring(4,6)+"-"+j.substring(6,8)+" "+j.substring(8,10)+":"+j.substring(10,12);
+        return dayss;
 	}
 	@Override
 	public String getCyrq(String pid){
-        return "1";
+        String k =ceshiDao.getCyrq(pid);
+        String daysss=k.substring(0,4)+"-"+k.substring(4,6)+"-"+k.substring(6,8)+" "+k.substring(8,10)+":"+k.substring(10,12);
+        return daysss;
 	}//出院日期
 	@Override
 	public String getFffs(String pid){
@@ -491,7 +500,13 @@ public class CeshiServiceImpl implements CeshiService {
     }
     @Override
     public String getBhxyssj(String pid){
-        return "1";
+        if(ceshiDao.getBhxyssj(pid)==null||ceshiDao.getBhxyssj(pid).isEmpty()){
+            return "null";
+        }else{
+            String k =ceshiDao.getBhxyssj(pid);
+            String daysss=k.substring(0,4)+"-"+k.substring(4,6)+"-"+k.substring(6,8)+" "+k.substring(8,10)+":"+k.substring(10,12);
+            return daysss;
+        }
     }//保护性约束时间
     @Override
     public Long getBhxyscs(String pid){
